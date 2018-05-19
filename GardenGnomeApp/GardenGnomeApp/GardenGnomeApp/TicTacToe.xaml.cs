59707f3a0696cec88890b9512d572d8549a676b6
Lewis Law <lewis_law@msn.com>
2018-05-19 23:27:52 +1000

    Tic Tac Toe 2 Player Mode using X and O

--- GardenGnomeApp/GardenGnomeApp/GardenGnomeApp/TicTacToe.xaml.cs
+++ GardenGnomeApp/GardenGnomeApp/GardenGnomeApp/TicTacToe.xaml.cs
@@ -16,7 +16,7 @@ namespace GardenGnomeApp
         {
             InitializeComponent();
         }
-
+        // Responsive Layout
         private double width = 0;
         private double height = 0;
 
@@ -31,17 +31,103 @@ namespace GardenGnomeApp
                 if (width > height)
                 {
                     outerStack.Orientation = StackOrientation.Horizontal;
-                    mainGrid.HeightRequest = (width/2+height)/2;
-                    mainGrid.WidthRequest = (width/2+height)/2;
-                    System.Diagnostics.Debug.WriteLine((width / 2 + height) / 2);
+                    mainGrid.HeightRequest = height;
+                    mainGrid.WidthRequest = height;
                 }
                 else
                 {
                     outerStack.Orientation = StackOrientation.Vertical;
                     mainGrid.HeightRequest = width;
-                    mainGrid.WidthRequest = height;
-                }       
+                    mainGrid.WidthRequest = width;
+                    
+                }
             }
+        }
+
+        //The Game
+        bool turn = true;
+        bool winner = false;
+        private void buttonClicked(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+            {
+                b.Text = "X";
+                b.FontSize = 25;
+            }
+            else
+            {
+                b.Text = "O";
+                b.FontSize = 25;
+            }
+            turn = !turn;
+            b.IsEnabled = false;
+            checkForWinner();
+        }
+
+        private void checkForWinner()
+        {
+            System.Diagnostics.Debug.WriteLine("CheckforWinner Working" + winner);
+            //Horizontal checks
+            if ((Button0_0.Text == Button1_0.Text) && (Button1_0.Text == Button2_0.Text) && (Button0_0.IsEnabled == false))
+            { winner = true; }
+            if ((Button0_1.Text == Button1_1.Text) && (Button1_1.Text == Button2_1.Text) && (Button0_1.IsEnabled == false))
+            { winner = true; }
+            if ((Button0_2.Text == Button1_2.Text) && (Button1_2.Text == Button2_2.Text) && (Button0_2.IsEnabled == false))
+            { winner = true; }
+
+            //Veritical Checks                                                                                 
+            if ((Button0_0.Text == Button1_0.Text) && (Button1_0.Text == Button2_0.Text) && (Button0_0.IsEnabled == false))
+            { winner = true; }
+            if ((Button0_1.Text == Button1_1.Text) && (Button1_1.Text == Button2_1.Text) && (Button0_1.IsEnabled == false))
+            { winner = true; }
+            if ((Button0_2.Text == Button1_2.Text) && (Button1_2.Text == Button2_2.Text) && (Button0_2.IsEnabled == false))
+            { winner = true; }
+
+            //Diagonal Checks                                                                                   
+            if ((Button0_0.Text == Button1_1.Text) && (Button1_1.Text == Button2_2.Text) && (Button0_0.IsEnabled == false))
+            { winner = true; }
+            if ((Button0_2.Text == Button1_1.Text) && (Button1_1.Text == Button2_0.Text) && (Button0_2.IsEnabled == false))
+            { winner = true; }
+            
+            //Declaring winner
+            if (winner == true)
+            {
+                string winnerName = "";
+                if (turn)
+                { winnerName = "2"; }
+                else
+                { winnerName = "1"; }
+
+                DisplayAlert("Game Over","Player " + winnerName + " Wins!", "OK");
+            }
+        }
+        
+        // restting the game with reset button
+        private void resetClicked()
+        {
+            Button0_0.Text = "";
+            Button1_0.Text = "";
+            Button2_0.Text = "";
+            Button0_1.Text = "";
+            Button1_1.Text = "";
+            Button2_1.Text = "";
+            Button0_2.Text = "";
+            Button1_2.Text = "";
+            Button2_2.Text = "";
+            Button0_0.IsEnabled = true;
+            Button1_0.IsEnabled = true;
+            Button2_0.IsEnabled = true;
+            Button0_1.IsEnabled = true;
+            Button1_1.IsEnabled = true;
+            Button2_1.IsEnabled = true;
+            Button0_2.IsEnabled = true;
+            Button1_2.IsEnabled = true;
+            Button2_2.IsEnabled = true;
+
+            turn = true;
+            winner = false;
+            System.Diagnostics.Debug.WriteLine("Check Variables turn: " + turn +" winner: " + winner);
         }
     }
 }
