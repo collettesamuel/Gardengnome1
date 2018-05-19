dd8bc0049a0cdd62916ecaf54653dddea59cc66c
Lewis Law <lewis_law@msn.com>
2018-05-19 18:13:27 +1000

    Responsive to orientation but not in proportion

--- GardenGnomeApp/GardenGnomeApp/GardenGnomeApp/TicTacToe.xaml.cs
+++ GardenGnomeApp/GardenGnomeApp/GardenGnomeApp/TicTacToe.xaml.cs
@@ -9,13 +9,39 @@ using Xamarin.Forms.Xaml;
 
 namespace GardenGnomeApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class TicTacToe : ContentPage
-	{
-		public TicTacToe ()
-		{
-			InitializeComponent ();
-		}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TicTacToe : ContentPage
+    {
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
 
-	}
+        private double width = 0;
+        private double height = 0;
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height); //must be called
+            if (this.width != width || this.height != height)
+            {
+                this.width = width;
+                this.height = height;
+                //reconfigure layout
+                if (width > height)
+                {
+                    outerStack.Orientation = StackOrientation.Horizontal;
+                    mainGrid.HeightRequest = (width/2+height)/2;
+                    mainGrid.WidthRequest = (width/2+height)/2;
+                    System.Diagnostics.Debug.WriteLine((width / 2 + height) / 2);
+                }
+                else
+                {
+                    outerStack.Orientation = StackOrientation.Vertical;
+                    mainGrid.HeightRequest = width;
+                    mainGrid.WidthRequest = height;
+                }       
+            }
+        }
+    }
 }
